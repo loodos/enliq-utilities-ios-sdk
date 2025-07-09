049c4d36b4b363885b64d52e04fe7ac6b66b436d
FatihTufann <167748615+fatihtufanloodos@users.noreply.github.com>
2025-07-09 14:29:37 +0300

    updated Package.swift file

--- Package.swift
+++ Package.swift
@@ -29,28 +29,28 @@ let package = Package(
         /// `EIQUtils` target
         .binaryTarget(
             name: "EIQUtils",
-            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.4/eiqutils.xcframework.zip",
+            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.5/eiqutils.xcframework.zip",
             checksum: "f7419131c78138cd57eb3ab409cfce939d18747e2a1a88e25f35139c6a2f3f50"
         ),
         
         /// `EIQJailbreakDetector` target
         .binaryTarget(
             name: "EIQJailbreakDetector",
-            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.4/eiqjailbreakdetector.xcframework.zip",
+            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.5/eiqjailbreakdetector.xcframework.zip",
             checksum: "3b7a2a5c419f02d2e8a434586533037a4034a186c6d13cb64011ac76f63a6339"
         ),
  
         /// `EIQOfflineControl` target
         .binaryTarget(
             name: "EIQOfflineControl",
-            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.4/eiqofflinecontrol.xcframework.zip",
+            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.5/eiqofflinecontrol.xcframework.zip",
             checksum: "4c681e0fbe0ec753fa8f7d0457d2a242911ad75df09d8808cd99f8c3b6323145"
         ),
  
         /// `EIQCommonUtils` target
         .binaryTarget(
             name: "EIQCommonUtils",
-            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.4/eiqcommonutils.xcframework.zip",
+            url: "https://github.com/loodos/enliq-utilities-ios-sdk/releases/download/0.0.5/eiqcommonutils.xcframework.zip",
             checksum: "b771e3ce0b810d1806ae80d7dcf5d79610af09683fd5a30ed164ab3e44b23910"
         )
     ]
